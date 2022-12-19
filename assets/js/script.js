9530c94e53773c0b6f21c56ece19020cb098ffd8
Murilo Lima <msl@cesar.school>
2022-12-19 21:57:13 +0000

    Add shuffle options

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,7 @@ function addClickEvent() {
     let answers = document.getElementsByClassName("answer");
     for (let answer of answers) {
         answer.addEventListener("click", function() {
-            checkAnswer(this.id);
+            checkAnswer(this.innerText);
         });
     }
 }
@@ -43,13 +43,13 @@ function addClickEvent() {
  */
 function checkAnswer(userAnswer) {
         
-    if (userAnswer.charAt(6) == fullQuestions[0].correct + 1){
+    if (userAnswer == fullQuestions[0].correct){
         alert("It is right. Congratulations!");
         incrementScore();
         nextQuestion();
         }
     else {
-        alert(`Sorry. The correct answer is ${fullQuestions[0].options[fullQuestions[0].correct]}`);
+        alert(`Sorry. The correct answer is ${fullQuestions[currentQuestionIndex].correct}`);
         incrementWrongAnswer();
         nextQuestion();
     }
@@ -82,6 +82,11 @@ function nextQuestion(){
         let fullQuestion = fullQuestions[currentQuestionIndex];
         let question = document.getElementById('question');
         question.textContent = fullQuestion.question;
+        
+        // shuffle options
+        shuffle(fullQuestion.options);
+
+        // display question and options
         let answer1 = document.getElementById('answer1');
         answer1.textContent = fullQuestion.options[0];
         let answer2 = document.getElementById('answer2');
@@ -100,91 +105,91 @@ let fullQuestions = [
 {
     "question" : "Tea originated in which country?",
     "options": ["India","Sri Lanka","United Kingdom","China"],
-    "correct": 3,
+    "correct": "China",
     "image": "https://img.buzzfeed.com/store-an-image-prod-us-east-1/XKazC-m86.png?downsize=625%3A*&output-format=auto&output-quality=auto"
 },
 {
     "question" : "Russia is the largest country in the world measured by land mass. What's the next largest?",
     "options": ["China","United States","Brazil", "Canada"],
-    "correct": 3,
+    "correct": "Canada",
     "image": "https://img.buzzfeed.com/store-an-image-prod-us-east-1/gLTnbXy3I.png?downsize=625%3A*&output-format=auto&output-quality=auto"
 },
 {
     "question" : "Croatia and Slovenia border which sea?",
     "options": ["Balearic Sea","Java Sea", "Laptev Sea", "Adriatic Sea"],
-    "correct": 3,
+    "correct": "Adriatic Sea",
     "image": "https://img.buzzfeed.com/store-an-image-prod-us-east-1/U30xdjJ6a.png?downsize=625%3A*&output-format=auto&output-quality=auto"
 },
 {
     "question" : "In Greek mythology, what was the name of the three-headed dog that guarded the gate to the underworld?",
     "options": ["Ipotane", "Griffin","Ophiotaurus","Cerberus"],
-    "correct": 3,
+    "correct": "Cerberus",
     "image": "https://img.buzzfeed.com/store-an-image-prod-us-east-1/C3clEUQBO.png?downsize=625%3A*&output-format=auto&output-quality=auto"
 },
 {
     "question" : "Where's the strongest human muscle located?",
     "options": ["The Shoulder", "The thigh","The buttocks","The jaw"],
-    "correct": 3,
+    "correct": "The jaw",
     "image": "https://img.buzzfeed.com/store-an-image-prod-us-east-1/eZWGBjEIG.png?downsize=625%3A*&output-format=auto&output-quality=auto"
 },
 {
     "question" : "Titan is a moon of which planet?",
     "options": ["Neptune","Uranus","Mercury","Saturn"],
-    "correct": 3,
+    "correct": "Saturn",
     "image": "https://img.buzzfeed.com/store-an-image-prod-us-east-1/wRURafEkp.png?downsize=625%3A*&output-format=auto&output-quality=auto"
 },
 {
     "question" : "What's the world's most expensive spice by weight?",
     "options": ["Mahlab","Vanilla bean","Cardamom","Saffron"],
-    "correct": 3,
+    "correct": "Saffron",
     "image": "https://img.buzzfeed.com/store-an-image-prod-us-east-1/4ZCQJWi_e.png?downsize=625%3A*&output-format=auto&output-quality=auto"
 },
 {
     "question" : "In what year did the French Revolution begin?",
     "options": ["1791","1785","1764","1789"],
-    "correct": 3,
+    "correct": "1789",
     "image": "https://img.buzzfeed.com/store-an-image-prod-us-east-1/r539lpUq_.png?downsize=625%3A*&output-format=auto&output-quality=auto"
 },
 {
     "question" : "What color dresses do Chinese women traditionally wear on their wedding day?",
     "options": ["White","Purple","Yellow","Red"],
-    "correct": 3,
+    "correct": "Red",
     "image": "https://img.buzzfeed.com/store-an-image-prod-us-east-1/flujU1a_B.png?downsize=625%3A*&output-format=auto&output-quality=auto"
 },
 {
     "question" : "Physicist Albert Einstein was born in which country?",
     "options": ["Belgium","Austria","Finland","Germany"],
-    "correct": 3,
+    "correct": "Germany",
     "image": "https://img.buzzfeed.com/store-an-image-prod-us-east-1/z6Cn0GSLV.png?downsize=625%3A*&output-format=auto&output-quality=auto"
 },
 {
     "question" : "Emerald is the traditional birthstone associated with which month of the year?",
     "options": ["March","August","October","May"],
-    "correct": 3,
+    "correct": "May",
     "image": "https://img.buzzfeed.com/store-an-image-prod-us-east-1/H4JkX2Ufi.png?downsize=625%3A*&output-format=auto&output-quality=auto"
 },
 {
     "question" : "Kampala is the capital of which East African country?",
     "options": ["Zambia","Rwanda","Ethiopia","Uganda"],
-    "correct": 3,
+    "correct": "Uganda",
     "image": "https://img.buzzfeed.com/store-an-image-prod-us-east-1/5OWNf_ent.png?downsize=625%3A*&output-format=auto&output-quality=auto"
 },
 {
     "question" : "Which ex-Beatle was born Richard Starkey in July 1940?",
     "options": ["Paul McCartney","John Lennon","George Harrison","Ringo Starr"],
-    "correct": 3,
+    "correct": "Ringo Starr",
     "image": "https://img.buzzfeed.com/store-an-image-prod-us-east-1/Rt67oi4Q7.png?downsize=625%3A*&output-format=auto&output-quality=auto"
 },
 {
     "question" : "Where was playwright Oscar Wilde born in October 1854?",
     "options": ["Galway, Ireland","Cork, Ireland","Limerick, Ireland","Dublin, Ireland"],
-    "correct": 3,
+    "correct": "Dublin, Ireland",
     "image": "https://img.buzzfeed.com/store-an-image-prod-us-east-1/mIBsv8iva.png?downsize=625%3A*&output-format=auto&output-quality=auto"
 },
 {
     "question" : "On which part of the body would one wear a cravat?",
     "options": ["Wrist","Head","Ankle","Neck"],
-    "correct": 3,
+    "correct": "Neck",
     "image": "https://img.buzzfeed.com/store-an-image-prod-us-east-1/Aj3kVGgi6.png?downsize=625%3A*&output-format=auto&output-quality=auto"
 }
 ];
